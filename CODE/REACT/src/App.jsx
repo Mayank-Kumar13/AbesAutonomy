97603be0ec58fb853a7030739db07a44b8ba6355
Mukul Yadav <mukul2007yadav@gmail.com>
2026-07-13 00:44:46 +0530

    Home page

--- CODE/REACT/src/App.jsx
+++ CODE/REACT/src/App.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
-import Navbar from './component/navbar/Navbar'
-import Credits from './pages/Credits'
+import React from 'react';
+import Navbar from './component/navbar/Navbar';
+import Credits from './pages/Credits';
+import HomeContent from './component/home/HomeContent';
 import { Routes, Route } from "react-router-dom";
+
 const App = () => {
   return (
-    <>
-    <Navbar />
-    <Routes>
-        
-        <Route path="/credits" element={<Credits />} />
-      </Routes>
-    </>
-  )
+    <div className="bg-[#080b0e] min-h-screen w-full flex flex-col">
+      
+      {/* Navbar stays full-width at the top */}
+      <Navbar />
+      
+      {/* This wrapper automatically limits the width and centers everything below the Navbar */}
+      <div className="w-full flex-grow flex flex-col">
+        <Routes>
+          <Route path="/" element={<HomeContent />} />
+          <Route path="/credits" element={<Credits />} />
+        </Routes>
+      </div>
+      </div>
+
+    
+  );
 }
-export default App
+
+export default App;
