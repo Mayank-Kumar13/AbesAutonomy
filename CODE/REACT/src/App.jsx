55d7173f9d370dfbde2edea0aee6478f6293e310
Mayank-Kumar13 <mayanktomer1306@gmail.com>
2026-07-13 09:15:59 +0530

    fix

--- CODE/REACT/src/App.jsx
+++ CODE/REACT/src/App.jsx
@@ -3,27 +3,6 @@ import Navbar from './component/navbar/Navbar';
 import Credits from './pages/Credits';
 import HomeContent from './component/home/HomeContent';
 import { Routes, Route } from "react-router-dom";
-
-
-const App = () => {
-  return (
-    <div className="bg-[#080b0e] min-h-screen w-full flex flex-col">
-      
-      {/* Navbar stays full-width at the top */}
-      <Navbar />
-      
-      {/* This wrapper automatically limits the width and centers everything below the Navbar */}
-      <div className="w-full flex-grow flex flex-col">
-        <Routes>
-          <Route path="/" element={<HomeContent />} />
-          <Route path="/credits" element={<Credits />} />
-        </Routes>
-      </div>
-      </div>
-
-    
-  );
-
 import Resources from './pages/Resources';
 import Amcat from './pages/Amcat';
 const App = () => {
@@ -31,6 +10,7 @@ const App = () => {
     <>
     <Navbar />
     <Routes>
+        <Route path="/" element={<HomeContent />} />
          <Route path="/resources" element={<Resources/>} />
         <Route path="/credits" element={<Credits />} />
         <Route path="/amcat" element={<Amcat />} />
